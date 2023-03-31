0441c7f33af3e9cddece2af4acf7c3ee2958814b
Vikash Pandey <pandeyvikash6991@gmail.com>
2023-03-31 22:28:55 +0530

    done

---
+++ assignment/assignment-9/assignment09-1/index.js
@@ -0,0 +1,7 @@
+
+ 
+document.getElementById("myButton").onclick = function(){
+    alert("please check your code");
+    var myWarning = document.getElementById("warning").innerHTML ="Please check your code ?";
+    console.log("please check your code",myWarning);
+}
---
+++ assignment/assignment-9/assignment09-2/index.js
@@ -0,0 +1,3 @@
+function onClick(){
+    document.getElementsByClassName("myDiv").style.background ="green";
+}
